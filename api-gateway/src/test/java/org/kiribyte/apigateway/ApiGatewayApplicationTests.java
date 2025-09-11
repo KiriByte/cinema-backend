608b75cfd6d4cf2e72f268d0ff50fd98eefa114d
Kirill Misyuro <kmisyuro@gmail.com>
2025-09-11 14:09:59 +0300

    init api-gateway

---
+++ api-gateway/src/test/java/org/kiribyte/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kiribyte.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
